7183418af43539db02a018b6ca262f0d0e21f447
Austin King <shout@ozten.com>
2013-09-12 13:03:56 -0700

    Tracking logged in user in localStorage

--- example/www/server.js
+++ example/www/server.js
@@ -1,21 +1,26 @@
+var verify = require('browserid-verify')();
 var express = require('express');
-var app = express();
-
 var path = require('path');
+
+var app = express();
+app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
     res.send('No much here... load owa.html as a file in your browser.');
 });
 
 app.post('/verify_persona_assertion', function(req, res) {
-  console.log('verify_persona_assertion');
-  res.send(JSON.stringify({
-    status: "okay",
-    email: "foo@bar.com"
-  }))
+    var assertion = req.body.assertion;
+    var audience = 'http://localhost:6660';
+    verify(assertion, audience, function(err, email, response) {
+        if (err) {
+            // make sure no session is created
+            console.log('ERROR: There was an error : ' + err);
+        }
+        res.send(JSON.stringify(response));
+    });
 });
 
-console.log('mapping', path.join(__dirname));
 app.use('/', express.static(path.join(__dirname)));
 
 app.listen(6660);
