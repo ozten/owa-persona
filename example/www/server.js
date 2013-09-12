5ef49f66b4b02dd6e6ddcf10ff9cfa8ec438dbca
Austin King <shout@ozten.com>
2013-09-11 22:17:34 -0700

    Hooking up events and crossing postMessage boundry

--- example/www/server.js
+++ example/www/server.js
@@ -7,8 +7,12 @@ app.get('/', function(req, res) {
     res.send('No much here... load owa.html as a file in your browser.');
 });
 
-app.post('/persona_assertion', function(req, res) {
-
+app.post('/verify_persona_assertion', function(req, res) {
+  console.log('verify_persona_assertion');
+  res.send(JSON.stringify({
+    status: "okay",
+    email: "foo@bar.com"
+  }))
 });
 
 console.log('mapping', path.join(__dirname));
